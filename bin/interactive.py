7b0a68202f1e804615a975ffe2f820f58903e254
Diana Spencer <diana.ponce.spencer@gmail.com>
2020-10-03 08:35:40 +0200

    clean code and comments

--- bin/interactive.py
+++ bin/interactive.py
@@ -1,39 +1,53 @@
 #!/usr/bin/env python
-import os,sys
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import argparse
+import os
+import sys
+import multiagent.scenarios as scenarios
 
 from multiagent.environment import MultiAgentEnv
 from multiagent.policy import InteractivePolicy
-import multiagent.scenarios as scenarios
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 if __name__ == '__main__':
-    # parse arguments
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('-s', '--scenario', default='simple.py', help='Path of the scenario Python script.')
     args = parser.parse_args()
 
-    # load scenario from script
     scenario = scenarios.load(args.scenario).Scenario()
-    # create world
+
     world = scenario.make_world()
-    # create multiagent environment
-    env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
-    # render call to create viewer window (necessary only for interactive policies)
+
+    # Create a multi-agent environment
+    env = MultiAgentEnv(
+        world,
+        reset_callback=scenario.reset_world,
+        reward_callback=scenario.reward,
+        observation_callback=scenario.observation,
+        info_callback=None,
+        shared_viewer=False
+    )
+
+    # Render call to create viewer window (necessary only for interactive policies)
     env.render()
-    # create interactive policies for each agent
-    policies = [InteractivePolicy(env,i) for i in range(env.n)]
-    # execution loop
+
+    # Create interactive policies for each agent
+    policies = [InteractivePolicy(env, i) for i in range(env.n)]
+
     obs_n = env.reset()
     while True:
-        # query for action from each agent's policy
+        # Collect experiences from all agents by querying
+        # actions from the current policies of each agent.
         act_n = []
         for i, policy in enumerate(policies):
             act_n.append(policy.action(obs_n[i]))
-        # step environment
+
+        # Perform actions from agents in the environment
         obs_n, reward_n, done_n, _ = env.step(act_n)
-        # render all agent views
+
+        # Render all agent views
         env.render()
-        # display rewards
-        #for agent in env.world.agents:
+
+        # Display rewards
+        # for agent in env.world.agents:
         #    print(agent.name + " reward: %0.3f" % env._get_reward(agent))
