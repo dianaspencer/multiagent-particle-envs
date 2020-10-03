3978994b9c324f515bfabe77128a6e3ac4e5c39d
Diana Spencer <diana.ponce.spencer@gmail.com>
2020-10-03 07:57:14 +0200

    code cleanup

--- multiagent/multi_discrete.py
+++ multiagent/multi_discrete.py
@@ -2,9 +2,7 @@
 # (https://github.com/openai/gym/blob/1fb81d4e3fb780ccf77fec731287ba07da35eb84/gym/spaces/multi_discrete.py)
 
 import numpy as np
-
 import gym
-#from gym.spaces import prng
 
 
 class MultiDiscrete(gym.Space):
@@ -35,13 +33,16 @@ class MultiDiscrete(gym.Space):
         randn = np.random.RandomState()
         random_array = randn.rand(self.num_discrete_space)
         return [int(x) for x in np.floor(np.multiply((self.high - self.low + 1.), random_array) + self.low)]
+
     def contains(self, x):
         return len(x) == self.num_discrete_space and (np.array(x) >= self.low).all() and (np.array(x) <= self.high).all()
 
     @property
     def shape(self):
         return self.num_discrete_space
+
     def __repr__(self):
         return "MultiDiscrete" + str(self.num_discrete_space)
+
     def __eq__(self, other):
         return np.array_equal(self.low, other.low) and np.array_equal(self.high, other.high)
